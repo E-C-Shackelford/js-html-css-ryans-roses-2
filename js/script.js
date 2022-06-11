603b283caecf1c7b4b64a1a7f34819993d05615f
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-10 17:57:15 -0700

    Initial commit: Change the color of the instagram and facebook icons, and apply a border and padding to the opening hours paragraph

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// change ig and fb icon colors
+var instagramIcon = document.querySelector(".fa-instagram");
+console.log(instagramIcon);
+instagramIcon.style.color = "#d45bb9";
+
+var facebookIcon = document.querySelector(".fa-facebook");
+console.log(facebookIcon);
+facebookIcon.style.color = "#6a99b5";
+
+//add a border and padding to the opening hours paragraph
+var openingHours = document.querySelector("footer p");
+console.log(openingHours);
+openingHours.style.border = "1px solid #d45bb9";
+openingHours.style.padding = "1em";
