aa13c3bef3793ae87cb0ee65eb7f921d4388ceae
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-10 18:27:05 -0700

    Comment out use of console.log

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 // change ig and fb icon colors
 var instagramIcon = document.querySelector(".fa-instagram");
-console.log(instagramIcon);
+//console.log(instagramIcon);
 instagramIcon.style.color = "#d45bb9";
+//console.log(instagramIcon);
 
 var facebookIcon = document.querySelector(".fa-facebook");
-console.log(facebookIcon);
+//console.log(facebookIcon);
 facebookIcon.style.color = "#6a99b5";
 
 //add a border and padding to the opening hours paragraph
 var openingHours = document.querySelector("footer p");
-console.log(openingHours);
+//console.log(openingHours);
 openingHours.style.border = "1px solid #d45bb9";
 openingHours.style.padding = "1em";
